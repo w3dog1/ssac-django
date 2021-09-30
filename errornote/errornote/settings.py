5e46c816c1f98bb869f9371b69c7bb19a89c5bc1
w3dog1 <86637046+w3dog1@users.noreply.github.com>
2021-09-30 17:27:14 +0900

    0930_2update

--- errornote/errornote/settings.py
+++ errornote/errornote/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'diary',
+    'userplate',
 ]
 
 MIDDLEWARE = [
@@ -100,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'userplate.User'
